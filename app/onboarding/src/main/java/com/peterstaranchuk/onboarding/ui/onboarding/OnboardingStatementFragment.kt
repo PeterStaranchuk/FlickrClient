7925351047c27c22d661f979891b2e010c6b1a46
peterstaranchuk <petr.staranchuk@gmail.com>
2021-08-13 23:56:07 +0300

    Make the description text to take 60% of the screen's width

--- app/onboarding/src/main/java/com/peterstaranchuk/onboarding/ui/onboarding/OnboardingStatementFragment.kt
+++ app/onboarding/src/main/java/com/peterstaranchuk/onboarding/ui/onboarding/OnboardingStatementFragment.kt
@@ -1,5 +1,6 @@
 package com.peterstaranchuk.onboarding.ui.onboarding
 
+import android.content.res.Resources
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,29 +10,31 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
+import android.util.DisplayMetrics
 
 class OnboardingStatementFragment : Fragment() {
 
     private val title by lazy { arguments?.getString(TITLE) ?: throw IllegalStateException("title should be passed in arguments") }
     private val description by lazy { arguments?.getString(DESCRIPTION) ?: throw IllegalStateException("description should be passed in arguments") }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return ComposeView(requireContext()).apply {
+            val dpInPx = context.resources.displayMetrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT
+            val widthInPixels = Resources.getSystem().displayMetrics.widthPixels.toFloat()
+            val padding = widthInPixels / dpInPx * 0.2f
+
             setContent {
-                Column(horizontalAlignment = CenterHorizontally, modifier = Modifier.padding(start = 40.dp, end = 40.dp)) {
+                Column(horizontalAlignment = CenterHorizontally) {
                     Text(
                         text = title,
                         color = Color.White,
@@ -46,7 +49,8 @@ class OnboardingStatementFragment : Fragment() {
                         color = Color.White,
                         textAlign = TextAlign.Center,
                         fontSize = 12.sp,
-                        fontFamily = FontFamily.SansSerif
+                        fontFamily = FontFamily.SansSerif,
+                        modifier = Modifier.padding(start = padding.dp, end = padding.dp)
                     )
                 }
             }
